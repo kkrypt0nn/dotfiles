6bf0e1e7856cbdeec9be8d47d34d2b88b0439aac
Krypton <root@krypton.ninja>
2024-10-27 15:43:06 +0100

    chore: Update homebrew.sh

--- homebrew.sh
+++ homebrew.sh
@@ -20,10 +20,12 @@ brew upgrade
 # Install my used packages
 
 ## 1337 stuff
+brew install exiftool
 brew install hashcat
 brew install hexedit
 brew install john
 brew install nmap
+brew install nyancat
 brew install radare2
 
 brew install --cask burp-suite
@@ -34,28 +36,31 @@ brew install --cask wireshark
 
 ## Programming stuff
 brew install angular-cli
-brew install go # The only and real programming language, change my mind.
-brew install hexo
+brew install go
 brew install kotlin
 brew install lua
 brew install mysql
 brew install node
 brew install openjdk
-brew install python
+brew install postgresql@16
+brew install python@3.12
+brew install python@3.13
 brew install rust
+brew install sqlite
 
 brew install --cask docker
-brew install --cask figma
 brew install --cask flutter
 brew install --cask github
+brew install --cask pgadmin4
+brew install --cask postman
 
 ## Documenting things
 brew install mkdocs
 brew install pandoc
 
 ## Code editors
-brew install --cask jetbrains-toolbox # Installing JetBrains IDEs via that
 brew install --cask visual-studio-code
+brew install --cask zed@preview
 
 ## Communication
 brew install --cask discord
@@ -63,8 +68,8 @@ brew install --cask threema
 brew install --cask whatsapp
 
 ## Random stuff
-brew install neofetch
 brew install pure
+brew install ripgrep
 brew install tree
 brew install wget
 
@@ -72,7 +77,10 @@ brew install --cask android-file-transfer
 brew install --cask font-jetbrains-mono-nerd-font
 brew install --cask gimp
 brew install --cask minecraft
+brew install --cask notion
 brew install --cask obs
+brew install --cask obsidian
+brew install --cask qbittorrent
 brew install --cask spotify
 brew install --cask steam
 brew install --cask the-unarchiver
